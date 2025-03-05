5e2fc34be360fd42f4f2014ee30643645735301d
Kasador <hunterstevenshaw@gmail.com>
2025-03-05 13:24:09 -0500

    🎯 Feat: Made our own API. Used Postman/URL to see data.

--- README.md
+++ README.md
@@ -43,3 +43,53 @@ npm run dev
 ### ╰┈➤
 
 <img width="600" alt="Image" src="https://github.com/user-attachments/assets/a2367ac3-efc3-4b05-bd02-6107de151b76" />
+
+### 🗂️ Let's make our **_own_** API. 
+### ╰┈➤
+
+```js
+const http = require('http'); // importing the http module from node:http
+
+const arrayList = [
+    {id: 1, item:  "item 1"},
+    {id: 2, item:  "item 2"},
+    {id: 3, item:  "item 3"},
+    {id: 4, item:  "item 4"}
+]; // array list
+
+const server = http.createServer((req, res) => { // using the createServer method from http module
+    /* setHeader Types >>> 
+        text/html
+        application/json
+        image/jpeg
+        image/png
+        image/gif
+        audio/mpeg
+        video/mp4
+        application/javascript
+        text/css
+        text/plain
+    */
+    res.setHeader('Content-Type', 'application/json'); // setting the response for the header
+
+    // res.write("<h1>h1 header</h1>"); // simple write response
+
+    res.end( // ending/final response
+        JSON.stringify( // This is to convert object to string, err: The "chunk" argument must be of type string or an instance of Buffer or Uint8Array.
+            {
+                sucess: true,
+                method: req.method,
+                data: arrayList
+            }
+        )
+    );
+});
+
+const PORT = 3000; // setting port number
+
+server.listen(PORT, () => { // listen to the port and then console log message
+    console.log(`Server running at http://localhost:${PORT}/`);
+})
+```
+
+<img width="523" alt="Image" src="https://github.com/user-attachments/assets/156b197a-a328-4773-975d-69220ed1a4d8" />
--- server.js
+++ server.js
@@ -1,16 +1,42 @@
-const http = require('http');
+const http = require('http'); // importing the http module from node:http
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>h1 header</h1>");
-    res.write("<h2>h2 header</h2>");
-    res.write("<h3>h3 header</h3>");
-//   res.end('Hello World\n');
-    res.end();
+const arrayList = [
+    {id: 1, item:  "item 1"},
+    {id: 2, item:  "item 2"},
+    {id: 3, item:  "item 3"},
+    {id: 4, item:  "item 4"}
+]; // array list
+
+const server = http.createServer((req, res) => { // using the createServer method from http module
+    /* setHeader Types >>> 
+        text/html
+        application/json
+        image/jpeg
+        image/png
+        image/gif
+        audio/mpeg
+        video/mp4
+        application/javascript
+        text/css
+        text/plain
+    */
+    res.setHeader('Content-Type', 'application/json'); // setting the response for the header
+
+    // res.write("<h1>h1 header</h1>"); // simple write response
+
+    res.end( // ending/final response
+        JSON.stringify( // This is to convert object to string, err: The "chunk" argument must be of type string or an instance of Buffer or Uint8Array.
+            {
+                sucess: true,
+                method: req.method,
+                data: arrayList
+            }
+        )
+    );
 });
 
-const PORT = 3000;
+const PORT = 3000; // setting port number
 
-server.listen(PORT, () => {
+server.listen(PORT, () => { // listen to the port and then console log message
     console.log(`Server running at http://localhost:${PORT}/`);
 })
