a77d07997145fbfeca06b5eb4f8fbef5430b7e19
Kasador <hunterstevenshaw@gmail.com>
2025-03-05 13:01:13 -0500

    Changing Content-Type will will tell the browser how to render out the responses.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<h1>h1 header</h1>");
+    res.write("<h2>h2 header</h2>");
+    res.write("<h3>h3 header</h3>");
+//   res.end('Hello World\n');
+    res.end();
 });
 
 const PORT = 3000;
